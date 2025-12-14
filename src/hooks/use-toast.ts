99963ee7938672601f491b8448440848c6f8d975
Raphael <contato@eversync.com.br>
2025-12-14 16:51:07 -0300

    Ajuste de erro

---
+++ src/hooks/use-toast.ts
@@ -0,0 +1,19 @@
+import { toast as sonnerToast } from "sonner"
+
+type ToastProps = {
+    title?: string
+    description?: string
+    variant?: "default" | "destructive"
+}
+
+export const useToast = () => {
+    const toast = ({ title, description, variant }: ToastProps) => {
+        if (variant === "destructive") {
+            sonnerToast.error(title, { description })
+        } else {
+            sonnerToast(title, { description })
+        }
+    }
+
+    return { toast }
+}
